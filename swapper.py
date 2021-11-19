1a2ad64f633913b901f55c5cde1651debf066bf9
Roneetshr <73659639+Roneetshr@users.noreply.github.com>
2021-11-19 18:50:00 +0000

    Add files via upload

---
+++ swapper.py
@@ -0,0 +1,11 @@
+Lizard = "Some lizards can detach their tails if they are caught by a predator.This helps them to get away quickly.When this happens, it’s called autotomy."
+Minecraft = "In 2009, Swedish programmer and designer Markus Persson set out to create a sandbox game—one that allows for free and organic exploration of a virtual world—for the launch of his new company, Mojang AB. Persson began work on what is now Minecraft on May 10 of that year, amending the product in increments until May 16. The “alpha version” of Minecraft made its public debut the very next day."
+Hello = input("What file do you want to open: ")
+
+print(Hello)
+if Hello == Lizard:
+    print(Minecraft)
+
+if Hello == Minecraft:
+    print(Lizard)
+    
